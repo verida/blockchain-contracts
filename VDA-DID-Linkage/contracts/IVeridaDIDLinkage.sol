7a32f9d58a70006f05850a75b7d06711cf2ccd6d
Alex <trust2ray@gmail.com>
2022-11-29 08:41:51 +0900

    Working : Not implemented yet

---
+++ VDA-DID-Linkage/contracts/IVeridaDIDLinkage.sol
@@ -0,0 +1,86 @@
+//SPDX-License-Identifier : MIT
+pragma solidity ^0.8.9;
+
+interface IVeridaDIDLinkage {
+    /**
+     * @notice Link a `did` to an `identifier`
+     * @dev Transaction check verification by VDA-Verification-Base contract
+     * @param did DID address to be linked
+     * @param identifier Identifier that will be linked to did
+     * @param signature Used to verify request
+     * @param signatureProof Used to verify request
+     */
+    function link(
+        address did, 
+        string calldata identifier, 
+        bytes calldata signature,
+        bytes calldata signatureProof) external;
+
+    /**
+     * @notice Unlink an `identifier` from a `did`
+     * @param did DID address from which `identifier` is unlinked
+     * @param identifier Identifier that is unlinked
+     */
+    function unlink(address did, string calldata identifier) external;
+
+    /**
+     * @notice Check whether an `identifier` is linked to a `did`
+     * @param did DID address
+     * @param identifier Identifier to be checked the linked status
+     * @return bool True if linked, false otherwise.
+     */
+    function isLinked(address did, string calldata identifier) external returns(bool);
+
+    /**
+     * @notice Add a identifierType
+     * @dev Only the owner of contract is allowed
+     * @param identifierTypeId Type of identifier to be added
+     * @param signerType Signer type of identifier type
+     */
+    function addIdentifierType(string calldata identifierTypeId, string calldata signerType) external;
+
+    /**
+     * @notice Add an trusted signer address
+     * @dev Only the owner of contract is allowed
+     * @param signer Signer address
+     */
+    function addTrustedSignerAddress(address signer) external;
+
+    /**
+     * @notice Remove a trusted signer address
+     * @dev Only the owner of contract is allowed
+     * @param signer Signer address
+     */
+    function removeTrustedSignerAddress(address signer) external;
+
+    /**
+     * @notice Check whether an address is trusted
+     * @dev Only owner of contract can check
+     * @param signer Signer address to be checked
+     */
+    function isTrustedSignerAddress(address signer) external view returns(bool);
+
+    /**
+     * @notice Struct for Identifier type
+     * @dev An `identifier` is inputed as a string and parsed to a struct
+     */
+    struct Identifier {
+        string kind;
+        string id;
+    }
+
+    /**
+     * @notice emitted when an `identifer` is linked to a `did`
+     * @param did DID address
+     * @param identifier Identifier to be checked the linked status
+     */
+    event Link(address did, string identifier);
+
+    /**
+     * @notice emitted when an `identifier` is unlinked from a `did`
+     * @param did DID address
+     * @param identifier Identifier to be checked the linked status
+     */
+    event Unlink(address did, string identifier);
+
+}
---
+++ VDA-DID-Linkage/contracts/VeridaDIDLinkage.sol
@@ -0,0 +1,127 @@
+//SPDX-License-Identifier : MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/structs/EnumerableSetUpgradeable.sol";
+
+import "./IVeridaDIDLinkage.sol";
+
+contract VeridaDIDLinkage is IVeridaDIDLinkage, OwnableUpgradeable {
+
+    using EnumerableSetUpgradeable for EnumerableSetUpgradeable.AddressSet;
+
+    EnumerableSetUpgradeable.AddressSet private _trustedSigners;
+
+    /**
+     * @notice Signer type for each identifier type
+     * @dev mapping of identifier type => signer type
+     */
+    mapping(string => string) private _identifierType;
+
+    /**
+    * @notice Initialize
+    */
+    function initialize() public initializer {
+    __Ownable_init();
+    }
+
+    /**
+     * @dev See {IVeridaDIDLinkage}
+     */
+    function addIdentifierType(string calldata identifierTypeId, string calldata signerType) external onlyOwner {
+        require(bytes(_identifierType[identifierTypeId]).length == 0, "Registered type");
+        require(signerType == "Self" || signerType == "Trusted", "Invalid signer type");
+
+        _identifierType[identifierTypeId] = signerType;
+    }
+
+    /**
+     * @dev See {IVeridaDIDLinkage}
+     */
+    function addTrustedSignerAddress(address signer) external onlyOwner {
+        require(!_trustedSigners.contains(signer), "Existing address");
+        _trustedSigners.add(signer);
+    }
+
+    /**
+     * @dev See {IVeridaDIDLinkage}
+     */
+    function removeTrustedSignerAddress(address signer) external onlyOwner {
+        require(_trustedSigners.contains(signer), "Invalid signer address");
+        _trustedSigners.remove(signer);
+    }
+
+    /**
+     * @dev See {IVeridaDIDLinkage}
+     */
+    function isTrustedSignerAddress(address signer) external view returns(bool) {
+        return _trustedSigners.contains(signer);
+    }
+
+    /**
+     * @notice Parse Identifier and returns kind & id
+     * @param identifier Identifier. Ex : "blockchain:eip155|0x0D10C68F52326C47Dfc3FDBFDCCb37e3b8C852Cb"
+     * @return string Identifier kind. Ex : "blockchain:eip155"
+     * @return string Identifier id. Ex: "eip155|0x0D10C68F52326C47Dfc3FDBFDCCb37e3b8C852Cb"
+     */
+    function parseIdentifier(string calldata identifier) internal pure returns(string memory, string memory) {
+        //0x7c
+        bytes memory strBytes = bytes(identifier);
+        require(strBytes.length > 0, "Invalid identifier");
+
+        uint len = strBytes.length;
+
+        uint sepPos = len;
+        uint index = 0;
+        uint8 sepCount = 0;
+        while (index < len && sepCount < 2) {
+            if (strBytes[index] == 0x7c) {
+                sepPos = index;
+                sepCount++;
+            }
+            index++;
+        }
+
+        require(index == len && sepCount == 1 && sepPos > 0 && sepPos < (len - 1), "Invalid identifier");
+
+        bytes memory kindBytes = new bytes(sepPos);
+        for (index = 0; index < sepPos; index++) {
+            kindBytes[index] = strBytes[index];
+        }
+
+        bytes memory idBytes = new bytes(len - sepPos - 1);
+        for (index = sepPos + 1; index < len; index++) {
+            idBytes[index - sepPos - 1] = strBytes[index];
+        }
+
+        string memory kind = string(kindBytes);
+        string memory id = string(idBytes);
+
+        return (kind, id);
+    }
+
+    /**
+     * Temporary function for test
+     */
+    function verifyRequest(address did, bytes memory params, bytes calldata signature, bytes calldata proof) internal {
+    }
+
+    /**
+     * @dev See {IVeridaDIDLinkage}
+     */
+    function link(
+        address did, 
+        string calldata identifier, 
+        bytes calldata signature,
+        bytes calldata signatureProof) external {
+
+        string memory kind;
+        string memory id;
+        (kind, id) = parseIdentifier(identifier);
+        require(bytes(_identifierType[kind]).length > 0, "Invalid identifier type");
+
+        
+        
+    }
+
+}
