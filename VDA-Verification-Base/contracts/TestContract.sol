8c596dd468e6c115afc7b7cfe9b09c6c737809de
Alex <trust2ray@gmail.com>
2022-12-01 18:50:03 +0900

    V 0.0.2 - Minor updates on review
    - Added comments to the functions
    - Move the verifyRequestWithArray(...) function to the TestContract as it's not used in the practice.

--- VDA-Verification-Base/contracts/TestContract.sol
+++ VDA-Verification-Base/contracts/TestContract.sol
@@ -14,6 +14,15 @@ contract TestContract is VDAVerificationContract {
         
     }
 
+    /**
+     * @notice Verify signature & proof. Message is generated from muliple arguments (name & value)
+     * @dev This function will get success if the signature & proof are correct
+     * @param did DID for nonce
+     * @param name Test parameter for signature
+     * @param value Test parameter for signature
+     * @param signature Signature of message
+     * @param proof Proof
+     */
     function testSign(address did, string calldata name, string calldata value, bytes calldata signature , bytes calldata proof) external {
         uint didNonce = getNonce(did);
         bytes memory paramData = abi.encodePacked(
@@ -28,6 +37,14 @@ contract TestContract is VDAVerificationContract {
         verifyRequest(did, validSigners, paramData, signature, proof);
     }
 
+    /**
+     * @notice Verify signature & proof. Message is only one parameter - rawData.
+     * @dev Almost same as testSign(...) function except function parameters.
+     * @param did DID for nonce
+     * @param rawData Message
+     * @param signature Signature of Message
+     * @param proof Proof
+     */
     function testRawStringData(address did, bytes calldata rawData, bytes calldata signature, bytes calldata proof) external {
         uint didNonce = getNonce(did);
         bytes memory _unsignedData = abi.encodePacked(
@@ -41,6 +58,58 @@ contract TestContract is VDAVerificationContract {
         verifyRequest(did, validSigners, _unsignedData, signature, proof);
     }
 
+    /**
+     * @notice Test function for `test/proof.ts`
+     * @dev This function works the same as verifyRequest(...) of VDAVerificationContract.sol
+     * @param did DID for nonce
+     * @param validSigners Available signers list one of which sign the proof
+     * @param params Parameter shows the message
+     * @param signature Signature of the message
+     * @param proof Proof
+     */
+    function verifyRequestWithArray(
+        address did, 
+        address[] calldata validSigners,
+        bytes calldata params, 
+        bytes calldata signature, 
+        bytes calldata proof
+    ) external {
+        require(validSigners.length > 0, "No signers available");
+
+        bytes32 paramsHash = keccak256(params);
+        address contextSigner = ECDSAUpgradeable.recover(paramsHash, signature);
+        string memory strContextSigner = StringsUpgradeable.toHexString(uint256(uint160(contextSigner)));
+
+        bool isVerified = false;
+        uint index = 0;
+
+        while (index < validSigners.length && !isVerified) {
+            address account = validSigners[index];
+            string memory strAccount = StringsUpgradeable.toHexString(uint256(uint160(account)));
+            bytes memory proofString = abi.encodePacked(
+                strAccount,
+                strContextSigner
+            );
+            bytes32 proofHash = keccak256(proofString);
+            address didSigner = ECDSAUpgradeable.recover(proofHash, proof);
+
+            if (didSigner == account){
+                isVerified = true;
+                break;
+            }
+            index++;
+        }
+
+        require(isVerified, "Invalid proof");
+        nonce[did]++;
+    }
+
+    /**
+     * @notice Get the signer from a inputed message - bytes type
+     * @param params Message of bytes type
+     * @param signature Signature of params
+     * @return address Signer of the message
+     */
     function recoverTest(bytes calldata params, bytes calldata signature) external pure returns(address){
         bytes32 paramHash = keccak256(params);
         address signer = ECDSAUpgradeable.recover(paramHash, signature);
@@ -48,6 +117,12 @@ contract TestContract is VDAVerificationContract {
         return signer;
     }
 
+    /**
+     * @notice Get the signer from a inputed message - string
+     * @param params Message of string
+     * @param signature Signature of params
+     * @return address Signer of the message
+     */
     function rawRecover(string calldata params, bytes calldata signature) external pure returns(address) {
         bytes32 paramHash = keccak256(bytes(params));
         address signer = ECDSAUpgradeable.recover(paramHash, signature);
@@ -55,6 +130,14 @@ contract TestContract is VDAVerificationContract {
         return signer;
     }
 
+    /**
+     * @notice Get the signer. Message is literal string of 2 addresses - `${addr1}${addr2}`
+     * @dev The signature was signed into raw string message. Here, we generate same string message from 2 addresses
+     * @param addr1 Address for message
+     * @param addr2 Address for message
+     * @param signature Signature of the message
+     * @return address Signer of the message
+     */
     function rawRecoverAddress(address addr1, address addr2, bytes calldata signature) external view returns(address) {
         string memory strAddr1 = StringsUpgradeable.toHexString(uint256(uint160(addr1)));
         string memory strAddr2 = StringsUpgradeable.toHexString(uint256(uint160(addr2)));
--- VDA-Verification-Base/contracts/VDAVerificationContract.sol
+++ VDA-Verification-Base/contracts/VDAVerificationContract.sol
@@ -15,18 +15,41 @@ contract VDAVerificationContract is OwnableUpgradeable {
 
     mapping(address => uint) internal nonce;
     
+    /**
+     * @notice Initializer for deploying the contract
+     * @dev This contract can't be deployed directly. Should be used as a parent class only
+     */
     function __VDAVerificationContract_init() internal onlyInitializing {
         __Ownable_init();
         __VDAVerificationContract_init_unchained();
     }
 
+    /**
+     * @notice Initializer for deploying the contract
+     * @dev Initialze the necessary stuffs those are unique to this contract
+     */
     function __VDAVerificationContract_init_unchained() internal onlyInitializing {
     }
 
+    /**
+     * @notice Get a nonce for DID
+     * @dev This is used to sign the message. It's for against replay-attack of the transactions
+     * @param did DID for nonce
+     * @return uint Current nonce of the DID
+     */
     function getNonce(address did) public view returns(uint) {
         return nonce[did];
     }
 
+     /**
+     * @notice Verify whether the request is valid. 
+     * @dev Verify the signature & proof signed by valid signers
+     * @param did DID for nonce
+     * @param validSigners Available signers list one of which sign the proof
+     * @param params Parameter shows the message
+     * @param signature Signature of the message
+     * @param proof Proof
+     */
     function verifyRequest(
         address did, 
         EnumerableSetUpgradeable.AddressSet storage validSigners,
@@ -64,40 +87,5 @@ contract VDAVerificationContract is OwnableUpgradeable {
         nonce[did]++;
     }
 
-    function verifyRequestWithArray(
-        address did, 
-        address[] calldata validSigners,
-        bytes calldata params, 
-        bytes calldata signature, 
-        bytes calldata proof
-    ) public {
-        require(validSigners.length > 0, "No signers available");
-
-        bytes32 paramsHash = keccak256(params);
-        address contextSigner = ECDSAUpgradeable.recover(paramsHash, signature);
-        string memory strContextSigner = StringsUpgradeable.toHexString(uint256(uint160(contextSigner)));
-
-        bool isVerified = false;
-        uint index = 0;
-
-        while (index < validSigners.length && !isVerified) {
-            address account = validSigners[index];
-            string memory strAccount = StringsUpgradeable.toHexString(uint256(uint160(account)));
-            bytes memory proofString = abi.encodePacked(
-                strAccount,
-                strContextSigner
-            );
-            bytes32 proofHash = keccak256(proofString);
-            address didSigner = ECDSAUpgradeable.recover(proofHash, proof);
-
-            if (didSigner == account){
-                isVerified = true;
-                break;
-            }
-            index++;
-        }
-
-        require(isVerified, "Invalid proof");
-        nonce[did]++;
-    }
+    
 }
