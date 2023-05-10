f113e7477b0d2f70bb80995a0d25cc877fdf7061
Alex <trust2ray@gmail.com>
2023-05-10 14:45:36 +0900

    [VDA-SBT] minor update in Contract file

--- VDA-SBT/contracts/SoulboundNFT.sol
+++ VDA-SBT/contracts/SoulboundNFT.sol
@@ -5,6 +5,7 @@ import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/IERC721Metad
 import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/StringsUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/structs/EnumerableSetUpgradeable.sol";
 
 import "@verida/vda-verification-contract/contracts/VDAVerificationContract.sol";
 import "@verida/common-contract/contracts/EnumerableSet.sol";
