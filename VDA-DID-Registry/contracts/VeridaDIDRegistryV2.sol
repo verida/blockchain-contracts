4ae6880fffc12ae894481c7fb2303d18662777b1
Alex <trust2ray@gmail.com>
2024-06-10 13:42:25 +0900

    [VDA-DID-Registry] v0.3.0
    - Added `DIDRegistryV2` contract
    - Add maual deployment script
    - Updated the Defender API key and secret key in the hardhat configuration

--- VDA-DID-Registry/contracts/VeridaDIDRegistryV2.sol
+++ VDA-DID-Registry/contracts/VeridaDIDRegistryV2.sol
@@ -5,7 +5,30 @@ import "./VeridaDIDRegistry.sol";
 
 /** @title VeridaDIDRegistry */
 contract VeridaDIDRegistryV2 is VeridaDIDRegistry {
+  using EnumerableSetUpgradeable for EnumerableSetUpgradeable.AddressSet;
+
+  /**
+   * @notice Return the version of the contract
+   */
   function getVersion() external pure virtual returns(string memory) {
     return "2.0";
+  }
+
+  /**
+   * @dev See {IVeridaDIDRegistry}
+   */
+  function getDIDs(uint startIndex, uint count) external view virtual override returns(address[] memory) {
+    if (count == 0 || (startIndex + count ) > _registeredDIDs.length()) {
+      revert OutOfRange();
+    }
+
+    address[] memory ret = new address[](count);
+
+    for (uint i; i < count;) {
+      ret[i] = _registeredDIDs.at(startIndex + i);
+      unchecked { ++i; }
+    }
+
+    return ret;
   }
 }
